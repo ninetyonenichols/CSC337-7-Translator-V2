4920f82a0006716ad77e471411c73d5c23a571ec
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-11 16:32:07 -0700

    Added functionality for responding to user-input.

--- server/public_html/updater.js
+++ server/public_html/updater.js
@@ -1,8 +1,13 @@
+/*
+ * Author: Justin Nichols
+ * Class: CSC337
+ * Purpose: Provides translated text whenever user-input is received.
+ */
 
+/*
+ * Provides translated text whenever user-input is received
+ */
 function translate() {
-  let origText = document.getElementById("orig-text").value;
-  let url='translate/' + origText;
-
   var httpRequest = new XMLHttpRequest();
   if (!httpRequest) {
     alert('Error!');
@@ -18,6 +23,14 @@ function translate() {
       }
     }
   };
+
+  // creating the url
+  let origText = document.getElementById("orig-text").value;
+  let origSel = document.getElementById("orig-lang");
+  let origLang = origSel.options[origSel.selectedIndex].value;
+  let foreignSel = document.getElementById("foreign-lang");
+  let foreignLang = foreignSel.options[foreignSel.selectedIndex].value;
+  let url=`translate/${origLang}/${foreignLang}/${origText}`;
 
   httpRequest.open('GET', url);
   httpRequest.send();
